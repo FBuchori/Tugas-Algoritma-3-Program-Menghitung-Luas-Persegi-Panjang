f725df4f515a4905290d065c029192770f6a8f42
Faisal Buchori <45659267+FBuchori@users.noreply.github.com>
2018-12-15 11:03:57 +0700

    Add files via upload

---
+++ Menghitung Luas Persegi Panjang.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int p,l,luas;
+printf("KAMPUS PELITA BANGSA - TEKNIK INFORMATIKA\n");
+printf("PROGRAM MENGHITUNG LUAS PERSEGI PANJANG\n");
+printf("NAMA 	: FAISAL BUCHORI\n");
+printf("NIM 	: 311810528\n\n");
+
+cout<<"masukan panjang:";
+cin>>p;
+cout<<"masukan lebar:";
+cin>>l;
+luas=p*l;
+cout<<"luas persegi panjang adalah "<<luas;
+return 0;
+}
+
